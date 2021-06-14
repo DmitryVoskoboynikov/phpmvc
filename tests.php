3236e3fbf7570416c6db8aaf742d527e42c1cf6e
Dmitry <dmitry.voskoboynikoff@gmail.com>
2021-06-14 18:09:50 +0300

    Create tests.php

---
+++ tests.php
@@ -0,0 +1,14 @@
+<?php
+
+//$name = "getUser";
+//echo $name;
+
+//preg_match_all("#^get([a-zA-Z0-9]+)$#", $name, $matches, PREG_PATTERN_ORDER);
+
+//print_r($matches);
+
+$pattern = preg_replace("#(:[a-zA-Z0-9]+)#", "([a-zA-Z0-9-_]+)", "users/edit/:i$
+echo $pattern;
+
+preg_match_all("#^{$pattern}$#", "users/edit/1/2", $values);
+print_r($values);
